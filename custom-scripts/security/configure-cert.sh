db3d47b435c8256d94f89f90e26f7d20ee685803
waltiam <2963975+waltiam@users.noreply.github.com>
2021-10-25 18:54:41 +0000

    🔧 tweeks

--- custom-scripts/security/configure-cert.sh
+++ custom-scripts/security/configure-cert.sh
@@ -5,16 +5,19 @@ tdir=$(mktemp --directory)
 pushd $tdir
 
 git init
-git config --global user.signingkey $(gpgsm --list-secret-keys | awk 'match($0,/0x/) {id =  substr($0, RSTART, 10)}END{print id}')
-git config --global user.email $(gpgsm --list-secret-keys | awk 'match($0,/aka:/) {id =  substr($0, RSTART+5)}END{print id}')
-git config --global gpg.program $(which gpgsm)
-git config --global gpg.format x509
+# git config --global user.signingkey $(gpgsm --list-secret-keys | awk 'match($0,/0x/) {id =  substr($0, RSTART, 10)}END{print id}')
+# git config --global user.email $(gpgsm --list-secret-keys | awk 'match($0,/aka:/) {id =  substr($0, RSTART+5)}END{print id}')
+# git config --global gpg.program $(which gpgsm)
+# git config --global gpg.format x509
 
-# git config --global user.signingkey $(gpg --list-secret-keys --keyid-format 0xlong | awk 'match($0,/0x/) {id =  substr($0, RSTART+2, 16)}END{print id}')
-# git config --global gpg.program $(which gpg)
+git config --global user.signingkey $(gpg --list-secret-keys --keyid-format 0xlong | awk 'match($0,/0x/) {id =  substr($0, RSTART+2, 16)}END{print id}')
+git config --global gpg.program $(which gpg)
 
+git config --global user.name $(gpg --list-secret-keys |  perl -n -e '/\[ultimate\]\s*([a-zA-Z]*)\s*/ && print $1')
+git config --global user.email $(gpg --list-secret-keys |  perl -n -e '/\[ultimate\].*<(.*)>/ && print $1')
 git config --global commit.gpgSign true
 git config --global tag.gpgSign true
+
 popd
 
 rm -rf $tdir
